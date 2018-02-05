9b1f57274f5b911709021bdb345b3eff40be3364
jarec <ormus395@gmail.com>
2018-02-05 01:31:20 -0700

    Refined it a bit

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,6 +5,7 @@ window.onload = (function() {
     products: [],
     cart: [],
     cartCount: 0,
+    cartPrice: 0,
     productList: document.querySelector('#productItems'),
     shoppingCart: document.querySelector('#cartItems'),
     cartCounter: document.querySelector('.cartCount')
@@ -22,11 +23,15 @@ window.onload = (function() {
     .then(() => {
       appState.products.map(product => {
         return output.push(`
-          <li class="item" value=${product.id}>${product.title}: ${product.price}</li>
+          <li class="item" value=${product.id}>
+            <img src=${product.img} />
+            ${product.title}: ${product.price}
+            <button class="add">+</button>
+          </li>
         `)
       })
       appState.productList.innerHTML = output.join('');
-      let items = Array.from(document.querySelectorAll('.item'));
+      let items = Array.from(document.querySelectorAll('.add'));
       items.forEach(item => {
         item.addEventListener('click', addToCart)
       });
@@ -39,11 +44,15 @@ window.onload = (function() {
     let {shoppingCart} = appState;
 
     cart.forEach(item => {
-      output.push(`<li class="cartItem" value=${item.id}>${item.title}: ${item.price} | ${item.quantity}</li>`)
+      output.push(`<li class="cartItem" value=${item.id}>
+        <img src=${item.img} />
+        ${item.title}: ${item.price} | ${item.quantity}
+        <button class="delete"> - </button>
+      </li>`)
     })
 
     shoppingCart.innerHTML = output.join('');
-    let cartItems = Array.from(document.querySelectorAll('.cartItem'));
+    let cartItems = Array.from(document.querySelectorAll('.delete'));
 
     cartItems.forEach(item => {
       item.addEventListener('click', removeFromCart)
@@ -55,7 +64,7 @@ window.onload = (function() {
   }
 
   function addToCart(e) {
-    let _id = e.target.value;
+    let _id = e.target.parentNode.value;
     let item = appState.products.find(isItem)
 
     function isItem(product) {
@@ -83,7 +92,7 @@ window.onload = (function() {
   }
 
   function removeFromCart(e) {
-    let _id = e.target.value;
+    let _id = e.target.parentNode.value;
     let item = appState.cart.find(isItem)
 
     function isItem(product) {
@@ -92,10 +101,17 @@ window.onload = (function() {
 
     for(let i = 0; i < appState.cart.length; i++) {
       if(appState.cart[i].id === _id){
-        appState.cartCount = appState.cartCount - item.quantity;
-        appState.cart.splice(i, 1)
-        cartAmount();
-        buildCart();
+        if(appState.cart[i].quantity > 1) {
+          appState.cart[i].quantity--
+          appState.cartCount--
+          cartAmount();
+          buildCart();
+        } else {
+          appState.cartCount = appState.cartCount - item.quantity;
+          appState.cart.splice(i, 1)
+          cartAmount();
+          buildCart();
+        }
       }
     }
   }
